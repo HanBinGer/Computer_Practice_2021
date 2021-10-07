7ff15eaf44fb1743d272e03f3eeaecc6e134004e
HanBinGer <35628064+HanBinGer@users.noreply.github.com>
2021-10-07 21:39:13 +0300

    Add files via upload

---
+++ Homework/3/ASCII_33-255.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+
+
+int main()
+{
+    char c;
+    for (int i = 33; i < 256; ++i) {
+        c = i;
+        std::cout << i << " - " << c<< std::endl;
+    }
+}
---
+++ Homework/3/F_Ackermann.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int akkerman(int m, int n) {
+    if (m == 0) return (n + 1);
+    if (m > 0 and n == 0) return akkerman(m - 1, 1);
+    if (m > 0 and n > 0) return akkerman(m - 1, akkerman(m, n - 1));
+}
+
+int main()
+{
+    int a, b;
+    std::cin >> a >> b;
+    std::cout << akkerman(a, b);
+}
